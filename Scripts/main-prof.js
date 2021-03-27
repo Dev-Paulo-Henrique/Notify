c985d9c0e0ec880044e83e0ae2897fecf596fdbf
Paulo Santos <67799522+Dev-Paulo-Henrique@users.noreply.github.com>
2021-03-27 14:50:58 -0300

    Update main-prof.js

--- Scripts/main-prof.js
+++ Scripts/main-prof.js
@@ -11,7 +11,7 @@ body = "<h1 id='title'>NOTIFY</h1>"
 body += "<div id='border'>"
 body += "<input id='text' type='text' placeholder='Digite seu nome' value=" + name + ">" + "<br>";
 body += "<textarea id='area' class='area' placeholder='Digite seu texto'></textarea>";
-body += "<select id='serie' class='area'>" + "<option disabled selected>" + "Informe a serie" + "</option>" + "<option>" + "1° A " + "</option>" + "</option>" + "<option>" + "1° B " + "</option>" + "<option>" + "2° A " + "</option>" + "<option>" + "2° B " + "</option>" + "<option>" + "3° A " + "</option>" + "<option>" + "3° B " + "</option>"  + "</select>"
+body += "<select id='serie' class='area'>" + "<option hidden>" + "Todos" + "</option>" + "<option>" + "1° A " + "</option>" + "</option>" + "<option>" + "1° B " + "</option>" + "<option>" + "2° A " + "</option>" + "<option>" + "2° B " + "</option>" + "<option>" + "3° A " + "</option>" + "<option>" + "3° B " + "</option>"  + "</select>"
 body += "<input id='next' onclick='sendMessage()' type='submit' value='Enviar'>";
 body += "</div>"
 body += "<fieldset id='fieldset'>" + "<ul id='notify'>" + "</ul>" + "</fieldset>"
