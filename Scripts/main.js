c33439301fe73da801acb092a2c3d3e40d12373f
Paulo Santos <67799522+Dev-Paulo-Henrique@users.noreply.github.com>
2021-03-31 15:11:15 -0300

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -6,11 +6,28 @@ body += "<select class='student' id='ano' >" + "<option disabled selected>" + "I
 body += "<input class='student' id='email' type='email' placeholder='Digite seu email' value='" + localStorage.getItem("email") + "'>"
 body += "<button onclick='sendName()' id='send'>Enviar</button>" + "<div id='options'>" +  "<button id='study' onclick='study()'>Aulas</button>" + "<button id='more' onclick='more()'>Chat</button>" + "</div>"
 body += "<fieldset id='set'>" + "<ul id='notify'>" + "</ul>" + "</fieldset>"
-
+body += "<p id='txt' hidden></p>"
 
 document.write(body);
 document.write(html);
 
+function time(){
+today = new Date();
+h = today.getHours();
+m = today.getMinutes();
+
+document.getElementById('txt').innerHTML = h + ":" + m;
+setTimeout('time()', 10000);
+if (h >= 13 && h <= 19) {
+  Swal.fire({
+    position: 'center',
+    icon: 'warning',
+    title: 'Está no turno de aula!',
+    showConfirmButton: false,
+    timer: 2000
+  })
+}
+}
 
 function more(){
   if(localStorage.getItem("email") && 
